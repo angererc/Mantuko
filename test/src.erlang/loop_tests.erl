b249dc0a348d3fe4c21569be03050f29ce75a7a1
chmaruni <christoph@mantuko.org>
2010-03-18 16:26:59 +0100

    nothing, really...

--- test/src.erlang/loop_tests.erl
+++ test/src.erlang/loop_tests.erl
@@ -4,4 +4,8 @@
 -include("include/values.hrl").
 
 loop_1_test() ->
+	%i:im(),
+	%i:ii(analyzer),
+	%i:iaa([init]),
+	%i:ib(analyzer, analyze, 2),
 	analyzer:analyze_file("test/src.mantuko/loop_1.masm", [{trace_verbosity, 9}]).
