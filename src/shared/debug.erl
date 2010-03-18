e19ea2282495937cfd0fb1897cf29a0baa8261e9
chmaruni <christoph@mantuko.org>
2010-03-18 19:29:21 +0100

    moved args_to_string and val_to_string into debug module

--- src/shared/debug.erl
+++ src/shared/debug.erl
@@ -1,6 +1,7 @@
 -module (debug).
 
 -export ([setup_tracing/0, fatal/2, warning/2, log/2, log_no_indent/2, debug/2]).
+-export ([args_to_string/1, val_to_string/1]).
 
 -define (FATAL, 1).
 -define (WARNING, 2).
@@ -44,3 +45,40 @@ display_string(Level, String, Params) ->
 		trace_verbosity, 
 		fun(OptLevel) -> OptLevel >= Level end, ?FATAL					
 	).
+	
+%use those to format long unreadable things into something shorter and nicer
+args_to_string([]) ->
+	"";
+args_to_string([Arg]) ->
+	val_to_string(1, Arg);
+args_to_string([Arg|Rest]) ->
+	val_to_string(1, Arg) ++ ", " ++ args_to_string(Rest).
+
+val_to_string(V) ->
+	val_to_string(1, V).
+		
+val_to_string(Depth, V) when Depth > 2, is_list(V) ->
+	io_lib:format("[...]", []);
+val_to_string(Depth, V) when Depth > 2, is_tuple(V) ->
+	io_lib:format("{...}", []);
+val_to_string(_Depth, []) ->
+	"[]";
+val_to_string(Depth, [One]) ->
+	io_lib:format("[~s]", [val_to_string(Depth+1, One)]);
+val_to_string(Depth, [One, Two]) ->
+	io_lib:format("[~s, ~s]", [val_to_string(Depth+1, One), val_to_string(Depth+1, Two)]);
+val_to_string(Depth, [One, Two, Three]) ->
+	io_lib:format("[~s, ~s, ~s]", [val_to_string(Depth+1, One), val_to_string(Depth+1, Two), val_to_string(Depth+1, Three)]);
+val_to_string(Depth, [One, Two|Rest]) ->
+	io_lib:format("[~s, ~s, <+~p>]", [val_to_string(Depth+1, One), val_to_string(Depth+1, Two), length(Rest)]);
+val_to_string(Depth, {One}) ->
+	io_lib:format("{~s}", [val_to_string(Depth+1, One)]);
+val_to_string(Depth, {One, Two}) ->
+	io_lib:format("{~s, ~s}", [val_to_string(Depth+1, One), val_to_string(Depth+1, Two)]);
+val_to_string(Depth, {One, Two, Three}) ->
+	io_lib:format("{~s, ~s, ~s}", [val_to_string(Depth+1, One), val_to_string(Depth+1, Two), val_to_string(Depth+1, Three)]);
+val_to_string(Depth, A) when is_tuple(A) ->
+	[F|R] = tuple_to_list(A),
+	io_lib:format("{~s, <+~p>}", [val_to_string(Depth+1, F), length(R)]);
+val_to_string(_Depth, A) ->
+	io_lib:format("~p", [A]).
--- src/shared/tracer.erl
+++ src/shared/tracer.erl
@@ -44,39 +44,6 @@ add_trace({M}) ->
 	add_trace({M, '_', '_'});
 add_trace(M) ->
 	add_trace({M, '_', '_'}).
-		
-args_to_string([]) ->
-	"";
-args_to_string([Arg]) ->
-	val_to_string(1, Arg);
-args_to_string([Arg|Rest]) ->
-	val_to_string(1, Arg) ++ ", " ++ args_to_string(Rest).
-	
-val_to_string(Depth, V) when Depth > 2, is_list(V) ->
-	io_lib:format("[...]", []);
-val_to_string(Depth, V) when Depth > 2, is_tuple(V) ->
-	io_lib:format("{...}", []);
-val_to_string(_Depth, []) ->
-	"[]";
-val_to_string(Depth, [One]) ->
-	io_lib:format("[~s]", [val_to_string(Depth+1, One)]);
-val_to_string(Depth, [One, Two]) ->
-	io_lib:format("[~s, ~s]", [val_to_string(Depth+1, One), val_to_string(Depth+1, Two)]);
-val_to_string(Depth, [One, Two, Three]) ->
-	io_lib:format("[~s, ~s, ~s]", [val_to_string(Depth+1, One), val_to_string(Depth+1, Two), val_to_string(Depth+1, Three)]);
-val_to_string(Depth, [One, Two|Rest]) ->
-	io_lib:format("[~s, ~s, <+~p>]", [val_to_string(Depth+1, One), val_to_string(Depth+1, Two), length(Rest)]);
-val_to_string(Depth, {One}) ->
-	io_lib:format("{~s}", [val_to_string(Depth+1, One)]);
-val_to_string(Depth, {One, Two}) ->
-	io_lib:format("{~s, ~s}", [val_to_string(Depth+1, One), val_to_string(Depth+1, Two)]);
-val_to_string(Depth, {One, Two, Three}) ->
-	io_lib:format("{~s, ~s, ~s}", [val_to_string(Depth+1, One), val_to_string(Depth+1, Two), val_to_string(Depth+1, Three)]);
-val_to_string(Depth, A) when is_tuple(A) ->
-	[F|R] = tuple_to_list(A),
-	io_lib:format("{~s, <+~p>}", [val_to_string(Depth+1, F), length(R)]);
-val_to_string(_Depth, A) ->
-	io_lib:format("~p", [A]).
 	
 trace_loop([_S1,_S2,_S3,_S4|Rest]=Indent) ->
 	receive
@@ -84,10 +51,10 @@ trace_loop([_S1,_S2,_S3,_S4|Rest]=Indent) ->
 			From ! {?MODULE, Indent},
 			trace_loop(Indent);
 		{trace, _, call, {M, F, Args}} ->
-			debug:log_no_indent(Indent ++ "Call: \e[1;31m~p:~p/~p\e[0;33m(~s)\e[30m", [M, F, length(Args), args_to_string(Args)]),
+			debug:log_no_indent(Indent ++ "Call: \e[1;31m~p:~p/~p\e[0;33m(~s)\e[30m", [M, F, length(Args), debug:args_to_string(Args)]),
 			trace_loop("    "++Indent);
 		{trace, _, return_from, {M, F, Arity}, Ret} ->
-			debug:log_no_indent(Rest ++ "Return: \e[31m~p:~p/~p \e[33m=> ~s\e[30m", [M, F, Arity, val_to_string(1, Ret)]),
+			debug:log_no_indent(Rest ++ "Return: \e[31m~p:~p/~p \e[33m=> ~s\e[30m", [M, F, Arity, debug:val_to_string(Ret)]),
 			trace_loop(Rest);
 		Other ->
 			debug:log_no_indent(Indent ++ "Other = ~p", [Other]),
