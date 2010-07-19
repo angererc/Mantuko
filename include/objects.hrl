707270fea153899bb3e83285a81910c0fe9bbc2b
chmaruni <christoph@mantuko.org>
2010-07-19 18:11:39 +0200

    cleaning out old xsched stuff

--- include/objects.hrl
+++
@@ -1,6 +0,0 @@
-%private to the object hierarchy; don't use those records directly, if possible
--record (struct, {reader, writer, slots}).
-
--record (array, {reader, writer, values}).
-
--record (lock, {reader, writer}).
--- src/utils/user_default.erl
+++
@@ -1,9 +0,0 @@
-%this file is loaded automatically into the erlang shell 
--module(user_default).
-
--export([la/0]).
-
-%reload all modules
-la() ->
-    Modules = [M || {M, P} <- code:all_loaded(), is_list(P) andalso string:str(P, "angererc") > 0],
-    [shell_default:l(M) || M <- Modules].
